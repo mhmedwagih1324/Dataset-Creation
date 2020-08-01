56add3771d74f36e0e482fbe9c0245c73e5c65ae
Mohamed Wagih <mhmedwagih1324@gmail.com>
2020-08-01 11:43:36 +0200

    haha

--- add_image_to_dataset.m
+++ add_image_to_dataset.m
@@ -1,12 +1,17 @@
 function [] = add_image_to_dataset( ~ )
 %Adds a captured image to dataset
-%   Detailed explanation goes here
-load('test.mat', 'imgColor', 'imgDepth');
-load('dataset.mat', 'images', 'depths');
-imgColor = reshape(imgColor, [1, 1080, 1920, 3]);
-imgDepth = reshape(imgDepth, [1, 424, 512]);
-images = [images;imgColor];
-depths = [depths;imgDepth];
-save('dataset.mat', 'images', 'depths');
+%   This function should run immediately after 'Capture_Image.m' to add a
+%   captured image saved in 'test.mat' to the dataset.mat
+pass = 'wigo';
+str = input('what are you doing?');
+if(strcmp(pass,str))
+    load('test.mat', 'imgColor', 'imgDepth');
+    load('dataset.mat', 'images', 'depths');
+    imgColor = reshape(imgColor, [1, 1080, 1920, 3]);
+    imgDepth = reshape(imgDepth, [1, 424, 512]);
+    images = [images;imgColor];
+    depths = [depths;imgDepth];
+    save('dataset.mat', 'images', 'depths');
+end
 end
 
